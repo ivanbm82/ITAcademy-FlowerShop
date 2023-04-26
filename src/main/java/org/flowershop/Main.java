d86885dfac06c1206d259917e3cc0f404a1b696c
SPortabella <sportabellam@gmail.com>
2023-04-26 10:13:45 +0200

    Review main

--- src/main/java/org/flowershop/Main.java
+++ src/main/java/org/flowershop/Main.java
@@ -1,11 +1,12 @@
 package org.flowershop;
 
+
 import org.flowershop.controller.TicketController;
 
 public class Main {
     public static void main(String[] args) {
 
-        TicketController ticketController = new TicketController();
+        TicketController ticketController = new org.flowershop.controller.TicketController();
 
         ticketController.ticketDataRequest();
 
